20a93729619fc2af6d3c0fef81035da6ff88f646
onkar ungratwar <ungratwaronkar@gmail.com>
2024-02-06 12:37:52 +0530

    chnageSME

--- src/services/survey.locations.service.js
+++ src/services/survey.locations.service.js
@@ -105,6 +105,8 @@ const getSchoolDataByMasterProjectIdAndCode = async (masterProjectId, role, code
       break;
     case 'division':
       filteredSchools = schools.filter((school) => school.Division.toLowerCase() === code.toLowerCase());
+      case 'SME':
+        filteredSchools = schools.filter((school) => school.block_cd_1 === data);
       break;
     // Add more cases as needed for other roles
     default:
